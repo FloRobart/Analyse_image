67a9971ca56682e56162140f0aa063259404768b
Floris <Floris.robart11@gmail.com>
2022-06-02 07:53:28 +0200

    maj main

--- Main.java
+++ Main.java
@@ -6,17 +6,21 @@ public class Main
 {
     public static void main(String[] args)
     {
-  try {
-    
-    Path file = Paths.get("./images/2020-09-08__13.09.50.jpeg");
-    BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+        try
+        {
+            Path path = Paths.get("./images/2020-09-08__13.09.50.jpeg");
+            BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
 
-    System.out.println("Date de création: " + attr.creationTime());
-    System.out.println("Date du dernier accès: " + attr.lastAccessTime());
-    System.out.println("Date derniere modification: " + attr.lastModifiedTime());
-    
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
-    }
+            System.out.println("creationTime: " + attr.creationTime());
+            System.out.println("lastAccessTime: " + attr.lastAccessTime());
+            System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
+
+            System.out.println("isDirectory: " + attr.isDirectory());
+            System.out.println("isOther: " + attr.isOther());
+            System.out.println("isRegularFile: " + attr.isRegularFile());
+            System.out.println("isSymbolicLink: " + attr.isSymbolicLink());
+            System.out.println("size: " + attr.size());
+            
+        } catch (Exception e) { System.out.println(e); }
     }
 }
